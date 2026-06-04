d972a8d931c5dd1ad849fb5f757d9ca0d08c1a38
Andres C. <acobos@cobtec.net>
2026-06-04 11:07:59 -0500

    Feat: multiple get examples

--- rocket-junio/src/main.rs
+++ rocket-junio/src/main.rs
@@ -16,12 +16,36 @@ fn get_item(id: usize, name: &str) -> String {
     format!("Item con ID: {} y Nombre: {}", id, name)
 }
 
+#[get("/items/<tenant>/<person_name>?<page>&<per_page>&<sort_by>")]
+fn get_items(
+    tenant: String,
+    person_name: String,
+    page: Option<u32>,
+    per_page: Option<u32>,
+    sort_by: Option<String>,
+) -> String {
+    let page = page.unwrap_or(1);
+    let per_page = per_page.unwrap_or(10);
+    let sort_by = sort_by.unwrap_or_else(|| String::from("id"));
+
+    format!(
+        "Petición recibida - Tenant: {}, Persona: {}, Página: {}, Registros por página: {}, Ordenado por: {}",
+        tenant, person_name, page, per_page, sort_by
+    )
+}
+
+#[get("/itemsMultiple?<ids>")]
+fn get_multiple_items(ids: Vec<String>) -> String {
+    format!("Fetching items with IDs: {:?}", ids)
+}
+
 #[launch]
 fn rocket() -> _ {
     // Configura el puerto 8090 de manera programática
     let config = rocket::Config::figment().merge(("port", 8090));
 
-    rocket::custom(config)
-        .mount("/", routes![index])
-        .mount("/api", routes![hello, get_item])
+    rocket::custom(config).mount("/", routes![index]).mount(
+        "/api",
+        routes![hello, get_item, get_items, get_multiple_items],
+    )
 }
